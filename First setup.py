de4858673ef43ded49a8037e0a4f0b0871b67c53
Warlord1f <Warlord1f@users.noreply.github.com>
2018-04-16 22:02:22 +0200

    Python
    Startup for the project
    
    Co-Authored-By: flyingspaghettimonstercode <flyingspaghettimonstercode@users.noreply.github.com>

---
+++ First setup.py
@@ -0,0 +1,14 @@
+## Machine learning
+## Author: Ferri Gierkink , Daan Slager
+## Date: 16-04-2018
+##============================================================================##
+## Version 0.0.0
+## Update Logg:
+##============================================================================##
+## Geinstalleerde PIP's :
+## python -m pip install -U pip setuptools
+## python -m pip install -U pip Pandas
+## python -m pip install -U pip xlrd
+## python -m pip install -U pip openpyx1
+##============================================================================##
+
